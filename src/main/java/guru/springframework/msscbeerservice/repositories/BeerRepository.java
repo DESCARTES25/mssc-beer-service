783d0465b658d854ecdda13bc5c5a068553a2864
Descartin <descartes25@hotmail.com>
2022-09-12 22:42:51 +0200

    Inicitial commit

---
+++ src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbeerservice.repositories;
+
+import guru.springframework.msscbeerservice.domain.Beer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+
+}
