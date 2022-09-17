7ac13fd8a3e5867a43c0e23b3916a79922033fb3
Descartin <descartes25@hotmail.com>
2022-09-17 09:55:55 +0200

    Validations

--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -3,6 +3,7 @@ package guru.springframework.msscbeerservice.web.controller;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -19,14 +20,14 @@ public class BeerController {
     }
 
     @PostMapping //POST create a new beer
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+    public ResponseEntity saveNewBeer(@RequestBody @Validated BeerDto beerDto){
 
         //todo imple
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping({"/{beerId}"}) //PUT modify beet
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody @Validated BeerDto beerDto){
         //todo imple
         return new ResponseEntity(HttpStatus.NO_CONTENT);
 
